5740c1fd5a494457928b7c7631eaa10d8bb143a7
Adam Abdurrahman <muhammad.adam.abd@gmail.com>
2026-02-05 07:42:07 +0700

    refactor: standardize icon sizing classes and adjust register page form layout.

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -80,7 +80,7 @@
           </UiFieldGroup>
 
           <UiButton type="submit" form="login-form" class="w-full" :disabled="isLoading">
-            <Loader2 v-if="isLoading" class="mr-2 h-4 w-4 animate-spin" />
+            <Loader2 v-if="isLoading" class="animate-spin" />
             Masuk
           </UiButton>
         </form>
@@ -95,7 +95,7 @@
         </div>
 
         <UiButton variant="outline" class="w-full" type="button">
-          <svg class="mr-2 h-4 w-4" viewBox="0 0 24 24">
+          <svg class="size-4" viewBox="0 0 24 24">
             <path
               d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
               fill="#4285F4"
